ac1fcd511d00c3ca3faec5a15bd1ae08bf35fab1
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-09-24 07:59:05 +0300

    tests passed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,16 @@
-import requests
 import json
 
-class GetRequester:
+import requests
 
+
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(url=self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        res_json = requests.get(url=self.url)
+        return res_json.json()
